3d55045ee5f92eaa109e93da951597be35a84d13
Skyyyr <emuswgluvr@gmail.com>
2022-05-23 14:09:33 -0700

    finished

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,30 @@
 function deafGrandma() {
+  //Call helper function
+  handleNextQuestion();
+}
 
+let bye = false;
+let outputMessage = "SPEAK UP, KID!";
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function handleNextQuestion() {
+  readline.question('HEY KID! ', input => {
+    if (input === "") {
+      outputMessage = "WHAT?!";
+    } else if (input.match(/^[GOODBYE]/)) {
+      if (bye) { return "LATER, SKATER!"; close();}
+      outputMessage = "LEAVING SO SOON?";
+      bye = true;
+    } else if (input.match(/^[AZ]/)) {
+      outputMessage = "NO, NOT SINCE 1946!";
+    }
+    console.log(outputMessage);
+    handleNextQuestion();
+  })
 }
 
 deafGrandma();
